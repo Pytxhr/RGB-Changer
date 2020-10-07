347837100b442a3332fd95ac7d67d03b75071a79
Jasmur <59015405+Jasmur@users.noreply.github.com>
2020-10-06 23:17:21 -0400

    Update RGBChanger.h

--- RGBChanger.h
+++ RGBChanger.h
@@ -1,32 +1,30 @@
 int red = 255, green = 0, blue = 255, rgb = 1;
-	bool rF = true, gF, bF = true, rF_, gF_, bF_;
-	void performRGBChange() {
-		switch (rgb) {
-			case 0: {
-				if (gF) {
-					green--;
-					if (!green) { gF = !gF;  rF_ = !rF_; }
-				break; }
-
-				if (red < 255 && rF_) red++;
-				else { rgb++; red = 255; rF = !rF; rF_ = !rF_; }
-			break; }
-			case 1: {
-				if (bF) {
-					blue--;
-					if (!blue) { bF = !bF; gF_ = !gF_; }
-				break; }
-
-				if (green < 255 && gF_) green++;
-				else { rgb++; green = 255; gF = !gF; gF_ = !gF_; }
-			break;}
-			case 2: {
-				if (rF) {
-					red--;
-					if (!red) { rF = !rF; bF_ = !bF_; }
-				break; }
-
-				if (blue < 255 && bF_) blue++;
-				else { rgb = 0; blue = 255; bF = !bF; bF_ = !bF_; }
-			}
-		}
+bool rF = true, gF, bF = true, rF_, gF_, bF_;
+void performRGBChange() {
+switch (rgb) {
+	case 0: {
+		if (gF) {
+			green--; 
+			if (!green) { gF = !gF;  rF_ = !rF_; }
+		break; }
+		
+		if (red < 255 && rF_) red++;
+		else { rgb++; red = 255; rF = !rF; rF_ = !rF_; }
+	break; } case 1: {
+		if (bF) {
+			blue--; 
+			if (!blue) { bF = !bF; gF_ = !gF_; }
+		break; }
+		
+		if (green < 255 && gF_) green++;
+		else { rgb++; green = 255; gF = !gF; gF_ = !gF_; }
+	break;} case 2: {
+		if (rF) {
+			red--; 
+			if (!red) { rF = !rF; bF_ = !bF_; }
+		break; }
+		
+		if (blue < 255 && bF_) blue++;
+		else { rgb = 0; blue = 255; bF = !bF; bF_ = !bF_; }
+	}
+}
